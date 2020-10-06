914c1ca61964e7633518719cb65c07c37a82b946
John Stefan <johnstefan93@hotmail.com>
2020-10-06 12:28:16 +0800

    First Commit

---
+++ src/home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './home.css';
+import * as RB from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function index() {
+    return (
+        <div>
+            <div className="section-1 text-center">
+                <RB.Container>
+                </RB.Container>
+            </div>
+            <div className="section-2 text-center">
+                <RB.Container>
+                </RB.Container>
+            </div>
+            <div className="section-3 text-center">
+                <RB.Container>
+                </RB.Container>
+            </div>
+            <div className="section-4 text-center">
+                <RB.Container>
+                </RB.Container>
+            </div>
+            <div className="section-5 text-center">
+                <RB.Container>
+                </RB.Container>
+            </div>
+        </div>
+    )
+}
+
+export default index;
