20e501f772562cc8c793818220228fec6e323331
John Stefan <johnstefan93@hotmail.com>
2020-10-06 16:44:11 +0800

    added popup form, and seperate sections

--- src/home.js
+++ src/home.js
@@ -3,11 +3,21 @@ import './home.css';
 import * as RB from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function index() {
+import { Container } from './Container/index';
+
+const index = () => {
+    const triggerText = 'Register Now for Free';
+    const onSubmit = (event) => {
+        event.preventDefault(event);
+        console.log(event.target.name.value);
+        console.log(event.target.email.value);
+    };
+
     return (
         <div>
             <div className="section-1 text-center">
                 <RB.Container>
+                    <Container triggerText={triggerText} onSubmit={onSubmit} />
                 </RB.Container>
             </div>
             <div className="section-2 text-center">
@@ -19,7 +29,15 @@ function index() {
                 </RB.Container>
             </div>
             <div className="section-4 text-center">
-                <RB.Container>
+                <RB.Container fluid>
+                    <RB.Row>
+                        <RB.Col className="column-1">
+                            Test 1
+                        </RB.Col>
+                        <RB.Col className="column-2" style={{color: "black"}}>
+                            Test 2
+                        </RB.Col>
+                    </RB.Row>
                 </RB.Container>
             </div>
             <div className="section-5 text-center">
