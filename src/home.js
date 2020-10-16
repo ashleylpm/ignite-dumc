01fc897b7cd96d3cf75c251c9f05de7092bdc917
John Stefan <johnstefan93@hotmail.com>
2020-10-16 15:21:33 +0800

    Added images and input into 4 sections

--- src/home.js
+++ src/home.js
@@ -2,47 +2,50 @@ import React from 'react';
 import './home.css';
 import * as RB from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import iYLSLogo from './images/ignite-logo/iYLS-logo.png';
 
 import { Container } from './Container/index';
+import {Figure} from "react-bootstrap";
+import FigureImage from "react-bootstrap/FigureImage";
 
 const index = () => {
-    const triggerText = 'Register Now for Free';
-    const onSubmit = (event) => {
-        event.preventDefault(event);
-        console.log(event.target.name.value);
-        console.log(event.target.email.value);
-    };
+    // const triggerText = 'Register Now for Free';
+    // const onSubmit = (event) => {
+    //     event.preventDefault(event);
+    //     console.log(event.target.name.value);
+    //     console.log(event.target.email.value);
+    // };
 
     return (
         <div>
             <div className="section-1 text-center">
-                <RB.Container>
-                    <Container triggerText={triggerText} onSubmit={onSubmit} />
-                </RB.Container>
+                <Figure className="text-align-left">
+                    <FigureImage
+                        width={600 + 'vh'}
+                        height={600 + 'vh'}
+                        className="iYLS-image"
+                        src={iYLSLogo}
+                    />
+                </Figure>
+                {/*<RB.Container>*/}
+                {/*    <Container triggerText={triggerText} onSubmit={onSubmit} />*/}
+                {/*</RB.Container>*/}
             </div>
             <div className="section-2 text-center">
-                <RB.Container>
-                </RB.Container>
             </div>
             <div className="section-3 text-center">
-                <RB.Container>
-                </RB.Container>
-            </div>
-            <div className="section-4 text-center">
                 <RB.Container fluid>
                     <RB.Row>
                         <RB.Col className="column-1">
-                            Test 1
                         </RB.Col>
                         <RB.Col className="column-2" style={{color: "black"}}>
-                            Test 2
                         </RB.Col>
                     </RB.Row>
                 </RB.Container>
             </div>
+            <div className="section-4 text-center">
+            </div>
             <div className="section-5 text-center">
-                <RB.Container>
-                </RB.Container>
             </div>
         </div>
     )
